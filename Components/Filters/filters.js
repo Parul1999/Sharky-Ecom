dd93c03b6c1df8032d3416e77aacf336348349a3
unknown <parulkohli1999@gmail.com>
2022-02-20 21:05:49 +0530

    Feat: Product Lising Page

---
+++ Components/Filters/filters.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+    document.getElementById("filter-navbar").style.width = "100%";
+    document.getElementById("filter-navbar").style.visibility = "visible";
+  }
+
+  function closeSidebar() {
+    document.getElementById("filter-navbar").style.width = "0%";
+    document.getElementById("filter-navbar").style.visibility = "hidden";
+  }
